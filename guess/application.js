f987371d6ee3e4e5f82126332416eac7b1681cf5
brigity5053 <brigity5053@kippnyccp.org>
2016-06-15 17:49:32 -0400

    Update application.js

--- guess/application.js
+++ guess/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
 	//create click event for submit button
+var randomNumber = Math.floor(Math.random() * 6 ) + 1;
 
+var guess = prompt('I am thinking of a random number between 1 and 6. What is it?');
+
+if (parseInt(guess) === randomNumber ) {
+
+  document.write('<p>You guessed the number!</p>');
+  
+}  else {
+
+
+  document.write('<p>Sorry. The number was ' + randomNumber + '</p>');
+
+}
 
 
 	//create click event for clear button
